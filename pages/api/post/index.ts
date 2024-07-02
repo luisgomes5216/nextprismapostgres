f1fbe288c81147e1e64b59903bce8755f8946ba7
luisgomes5216 <luisgomes5216@gmail.com>
2024-07-02 17:56:12 +0100

    currently with an error regarding the session

---
+++ pages/api/post/index.ts
@@ -0,0 +1,22 @@
+import { getSession } from 'next-auth/react';
+import prisma from '../../../lib/prisma';
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+  const session = await getSession({ req });
+  console.log("session log: ", session)
+
+  
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: "luisgomes5216@gmail.com" } },
+    },
+  });
+  res.json(result);
+}
